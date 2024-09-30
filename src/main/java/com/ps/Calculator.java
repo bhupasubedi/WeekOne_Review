87076635e9db4d2569ec6103407b2e8ef6b41a90
Bhupa Subedi <bhupa_subedi@yahoo.com>
2024-09-30 10:50:50 -0500

    first commit

---
+++ src/main/java/com/ps/Calculator.java
@@ -0,0 +1,36 @@
+package com.ps;
+
+class Calculator {
+
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+
+
+
+
+
+
+// static add method
+
+        public static int add(int num1, int num2){
+        public static int sum(int num 1+ int num2);
+        int add = sum(5, 10)
+
+
+           return num1 +num2;
+}
+
+    }
+
+
+// static subtract method
+
+
+// static multiplication method
+
+
+// static division method
+
+
+}
