68a45216458628aea98bb1f9ae92e11fb73a6769
Bhupa Subedi <bhupa_subedi@yahoo.com>
2024-09-30 11:27:01 -0500

    subtarct

--- src/main/java/com/ps/Calculator.java
+++ src/main/java/com/ps/Calculator.java
@@ -6,24 +6,24 @@ class Calculator {
     public static void main(String[] args) {
         int sum = addition( 200,500);
         System.out.println(sum);
+        int result = subtract(100,400);
+        System.out.println(result);
     }
 // add
         public static int addition(int numberOne, int numberTwo){
             return numberOne + numberTwo;
 
-
-
-
         }
 
 
-    }
-
-
 // static subtract method
-
+public static int subtract( int numOne, int numTwo ) {
+    return numOne - numTwo;
+}
 
 // static multiplication method
 
 
 // static division method
+
+}
